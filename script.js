b87fadede80f249617736d4cae15bd13e1edb0dd
Jared Reyes <jaredreyespt@gmail.com>
2023-05-11 17:02:05 +1000

    all operation logic done

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ const numbers = document.querySelectorAll('#number');
 const add = document.querySelector('#add');
 const subtract = document.querySelector('#subtract');
 const divide = document.querySelector('#divide');
+const multiply = document.querySelector('#multiply');
+const percentage = document.querySelector('#percentage');
+const invert = document.querySelector('#invert');
+const clear = document.querySelector('#clear');
 const equals = document.querySelector('#equals');
 
-
+// Memory
 let numerator = '';
-const operations = [];
+let operations = [];
 let total;
+
+//Events 
 
 numbers.forEach(number => {
   number.addEventListener('click', event => {
@@ -32,12 +38,40 @@ divide.addEventListener('click', event => {
   numerator = '';
 });
 
+multiply.addEventListener('click', event => {
+  operations.push(parseInt(numerator), event.target.textContent);
+  numerator = '';
+});
+
+percentage.addEventListener('click', () => {
+  const percentConversion = parseInt(numerator) / 100;
+  operations.push(percentConversion);
+  readOut.textContent = percentConversion;
+  numerator = '';
+});
+
+invert.addEventListener('click', () => {
+  const inverted = parseInt(numerator) * -1;
+  operations.push(inverted);
+  readOut.textContent = inverted;
+  numerator = '';
+});
+
+clear.addEventListener('click', () => {
+  operations = [];
+  numerator = '';
+  total = null;
+  readOut.textContent = 'Make a calculation...';
+});
+
 equals.addEventListener('click', () => {
   operations.push(parseInt(numerator));
   numerator = '';
   total = runOperations();
   readOut.textContent = total;
 });
+
+//Logic 
 
 function runOperations(){
  const sum = operations.reduce((accumulator, currentValue, currentIndex, array) => {
@@ -48,13 +82,10 @@ function runOperations(){
       accumulator -= nextValue;
     } else if (currentValue === '÷') {
       accumulator /= nextValue;
+    } else if (currentValue === 'x') {
+      accumulator *= nextValue;
     };
     return accumulator;
   });  
   return sum;
 } 
-
-  /*
-  Concatenate numbers until an operator is pressed
-  When an operator is pressed 
-  */
