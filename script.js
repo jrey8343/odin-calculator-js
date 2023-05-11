c30cded86281f06757b313b485960c72e2135220
Jared Reyes <jaredreyespt@gmail.com>
2023-05-11 16:03:51 +1000

    add, subtract, divide, equals

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const readOut = document.querySelector('#readOut');
 const numbers = document.querySelectorAll('#number');
 const add = document.querySelector('#add');
+const subtract = document.querySelector('#subtract');
+const divide = document.querySelector('#divide');
 const equals = document.querySelector('#equals');
 
 
 let numerator = '';
-const numerators = [];
-let total = 0;
-
+const operations = [];
+let total;
 
 numbers.forEach(number => {
   number.addEventListener('click', event => {
@@ -16,19 +17,42 @@ numbers.forEach(number => {
   })
 })
 
-add.addEventListener('click', () => {
-  // Take the current numerator string and parse it as a number
+add.addEventListener('click', event => {
+  operations.push(parseInt(numerator), event.target.textContent);
+  numerator = '';
+});
 
-  // Add it straight to the total
+subtract.addEventListener('click', event => {
+  operations.push(parseInt(numerator), event.target.textContent);
+  numerator = '';
+});
 
-
-})
+divide.addEventListener('click', event => {
+  operations.push(parseInt(numerator), event.target.textContent);
+  numerator = '';
+});
 
 equals.addEventListener('click', () => {
-  readOut.textContent = total;
+  operations.push(parseInt(numerator));
   numerator = '';
-})
+  total = runOperations();
+  readOut.textContent = total;
+});
 
+function runOperations(){
+ const sum = operations.reduce((accumulator, currentValue, currentIndex, array) => {
+  const nextValue = parseInt(array[currentIndex + 1]);
+    if (currentValue === '+') {
+      accumulator += nextValue;
+    } else if (currentValue === '-') {
+      accumulator -= nextValue;
+    } else if (currentValue === '÷') {
+      accumulator /= nextValue;
+    };
+    return accumulator;
+  });  
+  return sum;
+} 
 
   /*
   Concatenate numbers until an operator is pressed
