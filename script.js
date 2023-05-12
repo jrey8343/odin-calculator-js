0833bf340d32ff95963fbce1e0e9cfa319de982a
Jared Reyes <jaredreyespt@gmail.com>
2023-05-12 12:06:57 +1000

    added keyboard support and refactored operations eventListener

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const readOut = document.querySelector('#readOut');
 const numbers = document.querySelectorAll('#number');
-const add = document.querySelector('#add');
-const subtract = document.querySelector('#subtract');
-const divide = document.querySelector('#divide');
-const multiply = document.querySelector('#multiply');
+const operators = document.querySelectorAll('#operator');
 const percentage = document.querySelector('#percentage');
 const invert = document.querySelector('#invert');
 const clear = document.querySelector('#clear');
@@ -14,7 +11,7 @@ let numerator = '';
 let operations = [];
 let total;
 
-//Events 
+//Click Events 
 
 numbers.forEach(number => {
   number.addEventListener('click', event => {
@@ -23,35 +20,22 @@ numbers.forEach(number => {
   })
 })
 
-add.addEventListener('click', event => {
-  operations.push(parseInt(numerator), event.target.textContent);
-  numerator = '';
-});
-
-subtract.addEventListener('click', event => {
-  operations.push(parseInt(numerator), event.target.textContent);
-  numerator = '';
-});
-
-divide.addEventListener('click', event => {
-  operations.push(parseInt(numerator), event.target.textContent);
-  numerator = '';
-});
-
-multiply.addEventListener('click', event => {
-  operations.push(parseInt(numerator), event.target.textContent);
-  numerator = '';
-});
+operators.forEach(operator => {
+  operator.addEventListener('click', event => {
+    operations.push(parseFloat(numerator), event.target.textContent);
+    numerator = '';
+  })
+})
 
 percentage.addEventListener('click', () => {
-  const percentConversion = parseInt(numerator) / 100;
+  const percentConversion = parseFloat(numerator) / 100;
   operations.push(percentConversion);
   readOut.textContent = percentConversion;
   numerator = '';
 });
 
 invert.addEventListener('click', () => {
-  const inverted = parseInt(numerator) * -1;
+  const inverted = parseFloat(numerator) * -1;
   operations.push(inverted);
   readOut.textContent = inverted;
   numerator = '';
@@ -65,17 +49,57 @@ clear.addEventListener('click', () => {
 });
 
 equals.addEventListener('click', () => {
-  operations.push(parseInt(numerator));
+  operations.push(parseFloat(numerator));
   numerator = '';
   total = runOperations();
   readOut.textContent = total;
 });
 
-//Logic 
+//Keyboard Events
+
+window.document.addEventListener('keydown', event => {
+  if (event.key === '1' ||
+      event.key === '2' ||
+      event.key === '3' ||
+      event.key === '4' ||
+      event.key === '5' ||
+      event.key === '6' ||
+      event.key === '7' ||
+      event.key === '8' ||
+      event.key === '9' ||
+      event.key === '.') {
+        numerator += event.key;
+        readOut.textContent = numerator;
+      } else if (
+      event.key === '+' ||
+      event.key === '-' ||
+      event.key === '÷' ||
+      event.key === 'x') {
+        operations.push(parseFloat(numerator), event.key);
+        numerator = '';
+      } else if (event.key === '=') {
+        operations.push(parseFloat(numerator));
+        numerator = '';
+        total = runOperations();
+        readOut.textContent = total;
+      } else if (event.key === '%') {
+        const percentConversion = parseFloat(numerator) / 100;
+        operations.push(percentConversion);
+        readOut.textContent = percentConversion;
+        numerator = '';
+      } else if (event.key === 'Escape' || event.key === 'Backspace') {
+        operations = [];
+        numerator = '';
+        total = null;
+        readOut.textContent = 'Make a calculation...';
+      };
+})
+
+// Operational Logic
 
 function runOperations(){
  const sum = operations.reduce((accumulator, currentValue, currentIndex, array) => {
-  const nextValue = parseInt(array[currentIndex + 1]);
+  const nextValue = parseFloat(array[currentIndex + 1]);
     if (currentValue === '+') {
       accumulator += nextValue;
     } else if (currentValue === '-') {
